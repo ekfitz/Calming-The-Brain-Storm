d9658751ec129b5c274a2da16b4bb4397aa20286
Eric Fitz <57112072+ekfitz@users.noreply.github.com>
2021-02-04 13:44:13 -0700

    Add files via upload

---
+++ site_content.js
@@ -0,0 +1,34 @@
+//LARGE HEADER IMAGE
+logo_image = "assets/img/logo.jpg"
+
+
+//EMBEDED VIDEOS
+
+//Video 1
+video1_date = "December 10, 2020"
+video1_source = "https://www.youtube.com/embed/2CJcN0FUPnw"
+video1_title = "This Concept Will Help you to Actually Finish Something and Not be so Hard on Yourself!!"
+
+//Video2
+video2_date = "November 18, 2020"
+video2_source = "https://www.youtube.com/embed/Ou3jBXdcj64"
+video2_title = "I Totally Humiliated Myself Just to Say NO to Sugar"
+
+//Video3
+video3_date = "November 10, 2020"
+video3_source = "https://www.youtube.com/embed/UTg-l6-invM"
+video3_title = "I hired a life coach to get me off sugar"
+
+//Video4
+video4_date = "November 04, 2020"
+video4_source = "https://www.youtube.com/embed/H0M8BFuocQw"
+video4_title = "This is why you need a life coach ASAP"
+
+
+//ABOUT SECTION
+
+//Bio/Description
+about_bio = "Connecting with you on the highs and lows of being a momma.The host, Brittany Woods, a momma of 2 and part time nurse, talks about what she does and things she's learned to stay sane in her busy life.Brittany has gone from big city life and traveling nurse with her 2 babies to now settling into a small town.She will dive deep into topics like how she balances kids and hubby, self awareness and how to feel better.She is a woman that wants to connect with women like you that feel the same way."
+
+//Bio Image
+about_image = "assets/img/photo.jpg"
